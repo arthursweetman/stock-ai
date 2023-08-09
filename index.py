fb495f37a99ca29543437f9378e2ec7d1544b54d
Arthur Sweetman <56804555+arthursweetman@users.noreply.github.com>
2023-08-09 14:54:08 -0500

    Initialized basic files

---
+++ index.py
@@ -0,0 +1,17 @@
+"""
+Author: Arthur Sweetman
+
+Start Date: August 9, 2023
+
+This program is meant to be a predictive robot for the stock market to aid
+(and hopefully drive) financial gains.
+
+"""
+
+
+def main():
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    main()
