852db3032d3fb01779d2200b63463131189434a9
Arthur Sweetman <56804555+arthursweetman@users.noreply.github.com>
2023-08-17 13:11:18 -0500

    Added MACD, ATR, and RSI technical indicator data

--- stock_API.py
+++ stock_API.py
@@ -4,46 +4,57 @@ Module to be used in other files to import stock data from Yahoo Finance API
 
 from yahoo_fin.stock_info import get_data
 import pyfredapi as pf
+import requests
 import numpy as np
 import pandas as pd
 
-FRED_API_KEY = "492c019400f8f7963ee131c745be7685"
-
-# API key for the Alpha Vantage API
-# __API_KEY = "OR59ZKH3MZH9S2GU"
-
 start_date = "01/01/2001"
+ticker = "SPY"
+
+FRED_API_KEY = "492c019400f8f7963ee131c745be7685"
+# TAAPI_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjRkZTQzNDk0OThkNzVkYTM2ZTcxMzljIiwiaWF0IjoxNjkyMjg5NTU2LCJleHAiOjMzMTk2NzUzNTU2fQ.zkDmgI6goChvasL7PEuUaL2RGdIEXE_I-FaowG0A13E"
+ALPHA_VANTAGE_API_KEY = "OR59ZKH3MZH9S2GU"
+
+rsi_params = {
+    'function' : 'RSI',
+    'interval' : 'daily',
+    'time_period' : '14',
+    'series_type' : 'close',
+    'apikey' : ALPHA_VANTAGE_API_KEY
+}
+atr_params = {
+    'function' : 'ATR',
+    'interval' : 'daily',
+    'time_period' : '14',
+    'series_type' : 'close',
+    'apikey' : ALPHA_VANTAGE_API_KEY
+}
+macd_params = {
+    'function' : 'MACDEXT',
+    'interval' : 'daily',
+    'series_type' : 'close',
+    'apikey' : ALPHA_VANTAGE_API_KEY,
+    'fastmatype' : 1,
+    'slowmatype' : 1,
+    'signalmatype' : 1
+}
+
+def get_technical_indicator(params, symbol):
+    params['symbol'] = symbol.upper()
+    url = 'https://www.alphavantage.co/query?'
+    r = requests.get(url, params)
+    data = r.json()["Technical Analysis: " + params['function']]
+    df = pd.DataFrame.from_dict(data, orient = 'index')
+    return df
+
 
 def fred_date_format(old_format):
     return old_format[6:10] + "-" + old_format[0:2] + "-" + old_format[3:5]
 
-def rsi_calc(df, periods = 14, ema = True):
-    """
-    Returns a pd.Series with the relative strength index.
-    """
-    close_delta = df['close'].diff()
-
-    # Make two series: one for lower closes and one for higher closes
-    up = close_delta.clip(lower=0)
-    down = -1 * close_delta.clip(upper=0)
-    
-    if ema == True:
-	    # Use exponential moving average
-        ma_up = up.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
-        ma_down = down.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
-    else:
-        # Use simple moving average
-        ma_up = up.rolling(window = periods, adjust=False).mean()
-        ma_down = down.rolling(window = periods, adjust=False).mean()
-        
-    rsi = ma_up / ma_down
-    rsi = 100 - (100/(1 + rsi))
-    return rsi
-
-ticker = get_data("spy",
-                start_date = start_date,
-                index_as_date = True,
-                interval="1d")
+pricedat = get_data(ticker,
+                  start_date = start_date,
+                  index_as_date = True,
+                  interval="1d")
 
 VIX = get_data("^VIX",
                 start_date = start_date,
@@ -67,9 +78,17 @@ UMCSENT = pf.get_series(series_id = "UMCSENT",
                         api_key = FRED_API_KEY,
                         observation_start = fred_date_format(start_date))[['date','value']].set_index('date').rename(columns={'value':'UMCSENT'})
 
-__monthly = ticker.join([UNRATE, UMCSENT], how="outer")[['UNRATE', 'UMCSENT']].ffill()
-data = ticker.join([VIX, USDX, EFFR]).join(__monthly).dropna()
+MACD = get_technical_indicator(macd_params, ticker)[['MACD']]
+ATR = get_technical_indicator(atr_params, ticker)
+RSI = get_technical_indicator(rsi_params, ticker)
+
+MACD.index = pd.to_datetime(MACD.index)
+ATR.index = pd.to_datetime(ATR.index)
+RSI.index = pd.to_datetime(RSI.index)
+
+__monthly = pricedat.join([UNRATE, UMCSENT], how="outer")[['UNRATE', 'UMCSENT']].ffill()
+data = pricedat.join([VIX, USDX, EFFR, __monthly, MACD, ATR, RSI]).dropna()
 
 
 if __name__ == "__main__":
-    print(data)
+    pass
